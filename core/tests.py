1537819630d4b9353260a25bb27e3cfc111abe1a
zgfif <zihzag@gmail.com>
2023-07-17 07:15:18 +0000

    authorization: add tests to index page

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class IndexViewTests(TestCase):
+    def test_have_recent_words(self):
+        response = self.client.get('/')
+        text = 'Sign in'
+        self.assertContains(response, text=text, status_code=200)
+
+
+class SignUpViewTests(TestCase):
+    def test_have_form_to_signup(self):
+        response = self.client.get('/signup')
+        text = 'Password confirmation'
+        self.assertContains(response, text=text, status_code=200)
+
