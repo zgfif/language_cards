9c207aed56eb14b1f40e5f6598760d89aa63ab6d
zgfif <zihzag@gmail.com>
2023-08-24 17:16:30 +0300

    learning_page: add hiding the translated word

--- core/static/core/training.js
+++ core/static/core/training.js
@@ -1,15 +1,25 @@
-// event listener that shows and hides
-function toggleVisibilityOfCard() {
-    const training_div = document.getElementById('trainingBlock')
+//show html element
+function showHtmlElement(element) {
+    element.classList.remove('hiddenDiv')
+    element.classList.add('shownDiv')
+}
 
-    classes = training_div.classList
+//hide html element
+function hideHtmlElement(element) {
+    element.classList.add('hiddenDiv')
+    element.classList.remove('shownDiv')
+}
+
+
+// toggle visibility of element
+function toggleVisibilityOfCard() {
+    const card = document.getElementById('trainingBlock')
+    const classes = card.classList
 
     if (classes.contains('hiddenDiv')) {
-        classes.remove('hiddenDiv')
-        classes.add('shownDiv')
+        showHtmlElement(card)
     } else {
-        classes.remove('shownDiv')
-        classes.add('hiddenDiv')
+        hideHtmlElement(card)
     }
 }
 
@@ -17,14 +27,19 @@ function toggleVisibilityOfCard() {
 // function to check if translated is equal to entered word
 function checkTranslation() {
     const translation = document.querySelector('#translated span').textContent;
+    showHtmlElement(document.querySelector('#translated'))
 
     const entered = document.getElementById('guess').value;
 
     if (translation == entered) {
         console.log('Correct');
+
+
     } else {
         console.log('Incorrect');
     }
+    document.getElementById('guess').value = '';
+    showNextCard();
 }
 
 // event that run checking translation
@@ -34,5 +49,21 @@ check_button.addEventListener("click", checkTranslation, false);
 
 // event that shows and hides the "start training" button
 const start_button = document.getElementById('startTrainingButton');
+const training_div = document.getElementById('trainingBlock')
 start_button.addEventListener('click', toggleVisibilityOfCard, false);
 
+
+
+// return the list of learning words
+function retrieveWords() {
+    const cards_data = document.getElementById('wordsdata').textContent;
+    return Array(JSON.parse(cards_data))[0]
+}
+
+// fill the card with information
+function fillCard(know, dontKnow) {
+    document.querySelector('#word span').textContent = know;
+    document.querySelector('#translated span').textContent = dontKnow;
+}
+
+fillCard('стол', 'table')
