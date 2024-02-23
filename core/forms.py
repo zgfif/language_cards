94e25ed0a79ac7879d3258adb11a0135987e8620
zgfif <zihzag@gmail.com>
2024-02-23 16:28:18 +0200

    sign_in_up_ui: customize sign_up

--- core/forms.py
+++ core/forms.py
@@ -28,12 +28,19 @@ class SignInForm(forms.Form):
 
 
 class SignUpForm(forms.Form):
-    username = forms.CharField(required=True)
+    username = forms.CharField(required=True,
+                               widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'must be unique'}))
 
-    password = forms.CharField(widget=forms.widgets.PasswordInput(), required=True)
-    password_confirmation = forms.CharField(widget=forms.widgets.PasswordInput(), required=True)
+    password = forms.CharField(required=True,
+                               widget=forms.widgets.PasswordInput(attrs={'class': 'form-control',
+                                                                         'placeholder': 'make up any password'}),)
+    password_confirmation = forms.CharField(required=True,
+                                            widget=forms.widgets.PasswordInput(attrs={'class': 'form-control',
+                                                                                      'placeholder':
+                                                                                          'must the same as password'}))
 
     email = forms.EmailField(required=True,
+                             widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'mail@example.com'}),
                              validators=[validators.EmailValidator(message="Invalid Email")])
 
     def clean(self):
