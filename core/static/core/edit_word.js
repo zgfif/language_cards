39fee93e681c5ce356fe07557c6dae9f346638a0
zgfif <zihzag@gmail.com>
2024-02-25 08:36:54 +0200

    button_in_edit: perform refactoring

--- core/static/core/edit_word.js
+++ core/static/core/edit_word.js
@@ -1,78 +1,60 @@
 // this function makes the btn enabled
 function enableBtn(btn) {
-   btn.attr("disabled", false);
+   btn.attr('disabled', false);
 };
 
 
 // this function makes the btn disabled
 function disableBtn(btn) {
-    btn.attr("disabled", true);
+    btn.attr('disabled', true);
 };
 
 
 // assign event when the page is fully downloaded
 $(document).ready(function() {
+
+    // retrieving inputs
     const word = $('#id_word'),
           translation = $('#id_translation'),
           sentence = $('#id_sentence'),
           update_btn = $('#update_button');
 
-    const initWordValue = word.val(),
-          initTranslationValue = translation.val(),
-          initSentenceValue = sentence.val();
 
     // by default, "update" button is inactive
    disableBtn(update_btn);
 
-   var changed_inputs = [false, false, false];
+    // by default, all inputs in the form are not changed. This object is used to persist condition of three inputs:
+    // word, translation and sentence.
+   var changed_inputs ={ 'word': false, 'translation': false, 'sentence': false }
 
-    //  this event listener compares the initial value of "input_node" with its changed value
-    word.on( "input", function() {
-        new_value =$(this).val();
 
-        if (new_value == initWordValue) {
-            changed_inputs[0] = false
-        } else {
-            changed_inputs[0] = true
-        }
+   function assignInputListener(input_node, input_name) {
+        // retrieving default value of the input
+        const init_input_value = input_node.val();
 
-        if (changed_inputs[0] || changed_inputs[1] || changed_inputs[2]) {
-            enableBtn(update_btn);
-        } else {
-            disableBtn(update_btn);
-        }
-    });
+        // assign listener on the input will trigger when something changes in the input
+        input_node.on( 'input', function() {
 
-    translation.on( "input", function() {
-        new_value =$(this).val();
+            // retrieving the current value of the input
+            new_value =$(this).val();
 
-        if (new_value == initTranslationValue) {
-            changed_inputs[1] = false
-        } else {
-            changed_inputs[1] = true
-        }
+            // if the value of the input node is changed, it will update the condition of this input
+            if (new_value == init_input_value) {
+                changed_inputs[input_name] = false
+            } else {
+                changed_inputs[input_name] = true
+            }
 
-        if (changed_inputs[0] || changed_inputs[1] || changed_inputs[2]) {
-            enableBtn(update_btn);
-        } else {
-            disableBtn(update_btn);
-        }
-    });
+            // if, at least, one of the input states is changed the button becomes active
+            if (changed_inputs['word'] || changed_inputs['translation'] || changed_inputs['sentence']) {
+                enableBtn(update_btn);
+            } else {
+                disableBtn(update_btn);
+            }
+        });
+   }
 
-    sentence.on( "input", function() {
-        new_value =$(this).val();
-
-        if (new_value == initSentenceValue) {
-            changed_inputs[2] = false
-        } else {
-            changed_inputs[2] = true
-        }
-
-        if (changed_inputs[0] || changed_inputs[1] || changed_inputs[2]) {
-            enableBtn(update_btn);
-        } else {
-            disableBtn(update_btn);
-        }
-    });
+   assignInputListener(word, 'word');
+   assignInputListener(translation, 'translation');
+   assignInputListener(sentence, 'sentence');
 });
-
