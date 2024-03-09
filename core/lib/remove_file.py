8cc913a355d9cb415ffa541ffc6d9b9231e95ae5
zgfif <zizhag@gmail.com>
2024-03-09 14:04:39 +0200

    deployment: rewrite RemoveFile class

--- core/lib/remove_file.py
+++ core/lib/remove_file.py
@@ -4,6 +4,8 @@ from django.conf import settings
 
 # this class was created to remove any file stored in media folder by its filename
 class RemoveFile:
+    MEDIA_ROOT = settings.MEDIA_ROOT
+
     def __init__(self,  filename=''):
         self.filename = filename
 
@@ -14,5 +16,7 @@ class RemoveFile:
 
     # returns absolute path to path through MEDIA_ROOT directory
     def absolute_path_to_file(self):
-        if self.filename:
-            return settings.MEDIA_ROOT / str(self.filename)
+        if self.MEDIA_ROOT and self.filename:
+            return os.path.join(self.MEDIA_ROOT, str(self.filename))
+        else:
+            return ''
