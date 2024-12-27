a3017f4e603617ae1849d0ba5b0dd5ecbda99d3f
zgfif <zizhag@gmail.com>
2024-12-27 12:42:06 +0200

    dark_theme: done

--- core/static/core/js/snippets/navigation_bar.js
+++ core/static/core/js/snippets/navigation_bar.js
@@ -1,4 +1,18 @@
 $( document ).ready(function() {
+    let cookies = document.cookie.split(';');
+    
+    let obj = {}
+    for (const x of cookies) {
+        pair = x.split('=');
+        obj[pair[0]] = pair[1]
+    }
+
+    if (obj.hasOwnProperty(' theme')) {
+        const theme = obj[' theme'];
+        $('html:first').attr('data-bs-theme', theme);
+        $('#themeBtn').attr('src', '/static/core/images/' + theme + '_btn.png'); 
+    };
+
     $('#themeBtn').click((event) => {
         const html = $('html:first');
 
@@ -6,9 +20,11 @@ $( document ).ready(function() {
         if (html.attr('data-bs-theme') == 'dark') {
             html.attr('data-bs-theme', 'light');
             $(event.target).attr('src', '/static/core/images/light_btn.png');
+            document.cookie = "theme=light";
         } else {
             html.attr('data-bs-theme', 'dark');
             $(event.target).attr('src', '/static/core/images/dark_btn.png');
+            document.cookie = "theme=dark";
         };
     });
 });
