2a079238f05d960fb28b46fdec09074f994c8958
zgfif <zizhag@gmail.com>
2025-03-24 19:14:08 +0200

    studying progress: perform refactoring

--- core/lib/calculate_user_progress.py
+++ core/lib/calculate_user_progress.py
@@ -8,20 +8,31 @@ it calculates total progress, progress from native to studying language and prog
 """
 
 class CalculateUserProgress:
+    TOTAL = 'total'
+    STUDYING_TO_NATIVE = 'studying_to_native'
+    NATIVE_TO_STUDYING = 'native_to_studying'
+
     filters = {
-        'total': {'know_native_to_studying': True, 'know_studying_to_native':True},
-        'studying_to_native': {'know_studying_to_native': True},
-        'native_to_studying': {'know_native_to_studying': True},
+        TOTAL: {'know_native_to_studying': True, 'know_studying_to_native':True},
+        STUDYING_TO_NATIVE: {'know_studying_to_native': True},
+        NATIVE_TO_STUDYING: {'know_native_to_studying': True},
     }
 
     def __init__(self, user: User, studying_lang: StudyingLanguage) -> None:
+        """
+        Initialize the CalculateUserProgress instance.
+        """
         self.words = user.word_set.filter(studying_lang=studying_lang)
+        self.total_words_count = self.words.count()
     
     def perform(self, type_of_progress: str) -> Optional[float]:
-        if type_of_progress not in ['total', 'studying_to_native', 'native_to_studying']:
+        """
+        Calculate the progress based on the type of progress.
+        """
+        if type_of_progress not in self.filters:
             raise ValueError('Invalid type of progress')
         
-        if self.words.count() == 0:
+        if self.total_words_count == 0:
             return None
         
         known_count = self.words.filter(**self.filters[type_of_progress]).count() 
@@ -29,6 +40,9 @@ class CalculateUserProgress:
         return self.calculate_percentage(known_count)
         
     def calculate_percentage(self, known_count: int) -> float:
-        if self.words.count() == 0:
+        """
+        Calculate the percentage of known words.
+        """
+        if self.total_words_count == 0:
             return 0.0
-        return round(known_count / (self.words.count() / 100), 2)
+        return round(known_count / (self.total_words_count / 100), 2)
